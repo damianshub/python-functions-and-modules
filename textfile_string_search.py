5c7d3cf2760349aba15a4448008f0c8ab6f969b4
damianshub <59593432+damianshub@users.noreply.github.com>
2020-02-03 22:21:22 -0600

    Create textfile_string_search.py

---
+++ textfile_string_search.py
@@ -0,0 +1,19 @@
+file = open('filename.txt', 'r')
+lines = file.readlines()
+file.close()
+
+substring = 'is'
+
+print(f'Search the text for the following string: {substring}')
+print()
+total = 0
+linenum= 0
+for line in lines:
+    linenum += 1
+    line = line.split()
+    count = line.count(substring)
+    total += count
+    print(f'The count in line {linenum} is:', count)
+
+print()
+print(f'Total number of occurrences is:', total)
